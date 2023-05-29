9c79aa7dd29ccede3f84fc77c65f4a0fd4899439
jhonrs <jhonrs812@gmail.com>
2023-05-28 21:23:48 -0500

    Finish section about useReducer

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -6,12 +6,14 @@ import { todoReducer } from '../08-useReducer/todoReducer';
 const initialState = [];
 
 const init = () => {
-  return JSON.parse(localStorage.getItem("todos")) || [];
+    return JSON.parse(localStorage.getItem("todos")) || [];
 };
 
 export const useTodo = () => {
 
         const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+        
+
 
         useEffect(() => {
             localStorage.setItem("todos", JSON.stringify(todos) || []);
@@ -47,5 +49,7 @@ export const useTodo = () => {
         handleNewTodo,
         handleDeleteTodo,
         handleToggleTodo,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter( todo => !todo.done).length
     };
 };
