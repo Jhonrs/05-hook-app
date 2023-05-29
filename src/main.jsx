907628ba9e54f3d0fe31d9e6b8d9a52328a78695
jhonrs <jhonrs812@gmail.com>
2023-05-29 15:44:00 -0500

    Finish Section useContext

--- src/main.jsx
+++ src/main.jsx
@@ -14,14 +14,18 @@ import ReactDOM from 'react-dom/client';
 /* import { CallbackHook } from './06-memos/CallBackHook'; */
 /* import { Padre } from './07-tarea-memo/Padre'; */
 /* import './08-useReducer/intro-reducer'; */
-import { TodoApp } from './08-useReducer/TodoApp';
+/* import { TodoApp } from './08-useReducer/TodoApp'; */
+import { MainApp } from './09-useContext/MainApp';
 
 
 
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- // <React.StrictMode>
-    <TodoApp />
-  //</React.StrictMode>,
+ <BrowserRouter>
+  {/* // <React.StrictMode> */}
+    <MainApp />
+  {/* //</React.StrictMode>, */}
+ </BrowserRouter>
 )
